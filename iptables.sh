b7ac7bf403bb8ef28602cc974247214c78b487bf
develadam1 <develadam@gmail.com>
2019-08-28 13:08:56 +0100

    opened services ports

--- iptables.sh
+++ iptables.sh
@@ -17,6 +17,13 @@ iptables -A INPUT ! -i lo -s 127.0.0.0/8 -j REJECT
 # Accept all established traffic.
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# Allow HTTP server & SSL.
+iptables -A INPUT -i eth0 -p tcp -m state --state NEW --dport 80 -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -m state --state NEW --dport 443 -j ACCEPT
+
+# Allow Postfix.
+iptables -A INPUT -i eth0 -p tcp -m state --state NEW --match multiport --dports 25,465,587,143,993 -j ACCEPT
+
 # Unlock services port.
 iptables -A INPUT -i eth0 -p tcp -m state --state NEW --dport 3000 -j ACCEPT
 
